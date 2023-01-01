95d316682445211cb3be2e175496d6d493cae317
Elisa Andrade <elisalvsan@gmail.com>
2023-01-01 12:29:15 -0300

    finalizando o CArd components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 function App() {
   return (
-    <div className="flex w-full h-screen items-center justify-center">
+    <div className="flex flex-col w-full h-screen items-center justify-center">
       <Form />
+      <Card />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-function Card(){
-    return ();
+function Card() {
+  return (
+    <div className="bg-white p-6 mt-10 rounded-lg shadow-md">
+      <div className="text-center">
+        <span className="block text-xl font-bold text-slate-700">
+          Nome da Cidade
+        </span>
+        <span className="text-slate-400 text-md font-medium">Estado, País</span>
+      </div>
+
+      <div className="font-bold text-slate-700 flex mt-4 mb-2">
+        <span className="text-8xl">25</span>
+        <span className="text-3xl mt-2">°C</span>
+      </div>
+
+      <div className="text-center">
+        <span className="block">Ícone</span>
+        <span className="text-slate-700 font-medium">Nublado</span>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
